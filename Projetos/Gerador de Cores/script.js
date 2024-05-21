c1c73543e9fefde064adfac982569a0ab28a38ba
Brunoobert <131983394+Brunoobert@users.noreply.github.com>
2024-05-20 22:22:31 -0300

    v2

--- Projetos/Gerador de Cores/script.js
+++ Projetos/Gerador de Cores/script.js
@@ -2,7 +2,7 @@ const cor = document.querySelector(".color");
 const btnMudar = document.getElementById("btn-mudar");
 const btnCopiar = document.getElementById("btn-copiar");
 
-const cores = ["red", "green", "lightblue", "#f15025", "#6C0B47", "#B23CFD", "#7DFC6B", "#4A871E", "#FABE81", "#2D8EF1", "#E04C7C", "#9A2F10", "#1BCDAB", "#D8C301"];
+const cores = [ "#f15025", "#6C0B47", "#B23CFD", "#7DFC6B", "#4A871E", "#FABE81", "#2D8EF1", "#E04C7C", "#9A2F10", "#1BCDAB", "#D8C301"];
 
 btnMudar.addEventListener('click', function(){
     
