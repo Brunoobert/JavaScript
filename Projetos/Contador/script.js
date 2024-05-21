cbfec261f043af9fef3df63d546614b44b0d3e12
Brunoobert <131983394+Brunoobert@users.noreply.github.com>
2024-05-21 13:50:04 -0300

    v2

--- Projetos/Contador/script.js
+++ Projetos/Contador/script.js
@@ -1,47 +1,47 @@
 const btnIncrementar = document.getElementById('btn-incrementar');
-        const btnZerar = document.getElementById('btn-zerar');
-        const btnDecrementar = document.getElementById('btn-decrementar');
-        const numero = document.getElementById('number');
-        const passo = document.getElementById('passo'); 
+    const btnZerar = document.getElementById('btn-zerar');
+    const btnDecrementar = document.getElementById('btn-decrementar');
+    const numero = document.getElementById('number');
+    const passo = document.getElementById('passo'); 
 
-        function atualizarCor(){
-            let count = parseInt(numero.textContent);
-            if(count == 0){
-                numero.style.color = 'black';
-            }else if(count > 0){
-                numero.style.color = 'green';
-            }else{
-                numero.style.color = 'red';
-            }
+    function atualizarCor(){
+        let count = parseInt(numero.textContent);
+        if(count == 0){
+            numero.style.color = 'black';
+        }else if(count > 0){
+            numero.style.color = 'green';
+        }else{
+            numero.style.color = 'red';
         }
+    }
 
-        btnIncrementar.addEventListener('click', function(){
-            let count = parseInt(numero.textContent);
-            let passoIncrementar = parseInt(passo.value);
-            if(passoIncrementar != 0){
-                count += passoIncrementar;
-            }else{
-                count++;
-            }
-            numero.textContent = count;
-            atualizarCor();
-        });
-
-        btnZerar.addEventListener('click', function(){
-            numero.textContent = 0;
-            atualizarCor();
-        });
-
-        btnDecrementar.addEventListener('click', function(){
-            let count = parseInt(numero.textContent);
-            let passoDecrementar = parseInt(passo.value); 
-            if(passoDecrementar != 0){
-                count -= passoDecrementar;
-            }else{
-                count--;
-            }
-            numero.textContent = count;
-            atualizarCor();
-        });
-
+    btnIncrementar.addEventListener('click', function(){
+        let count = parseInt(numero.textContent);
+        let passoIncrementar = parseInt(passo.value);
+        if(passoIncrementar != 0){
+            count += passoIncrementar;
+        }else{
+            count++;
+        }
+        numero.textContent = count;
         atualizarCor();
+    });
+
+    btnZerar.addEventListener('click', function(){
+        numero.textContent = 0;
+        atualizarCor();
+    });
+
+    btnDecrementar.addEventListener('click', function(){
+        let count = parseInt(numero.textContent);
+        let passoDecrementar = parseInt(passo.value); 
+        if(passoDecrementar != 0){
+            count -= passoDecrementar;
+        }else{
+            count--;
+        }
+        numero.textContent = count;
+        atualizarCor();
+    });
+
+    atualizarCor();
